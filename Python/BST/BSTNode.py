970206c9679e792467deccccf9a14e549c558901
alui07 <alex.lui858@gmail.com>
2019-11-14 11:36:50 -0800

    some boilerplate BST

---
+++ Python/BST/BSTNode.py
@@ -0,0 +1,17 @@
+import sys, os
+
+class BSTNode:
+    left = None
+    right = None
+    val = 0
+
+    def __init__(self, val = 0):
+        self.val = val
+
+        
+
+def main(argv):
+    return
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ Python/BST/BSTTree.py
@@ -0,0 +1,9 @@
+import os, sys, random
+from BSTNode import BSTNode
+
+class BSTTree:
+    root = None
+
+    def __init__(self):
+        return
+        
